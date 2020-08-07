112593f5e78fd316a12a7d50629735c648fab403
TimChen <timchen@kkstream.com.tw>
2020-08-07 11:56:29 +0800

    Update gitignore file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Player } from 'bitmovin-player';
+import Config from './Config';
+
+class PlayerWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  componentDidMount() {
+    var player;
+
+    var conf = {
+        key: Config.license
+    };
+    var source = {
+        dash: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+        hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+        progressive: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
+        poster: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
+    };
+
+    player = new Player(document.getElementById('player'), conf);
+    console.log("License: " + Config.license)
+    player.load(source).then(function () {
+        console.log('Successfully loaded source '); // Success!
+        //player.play()
+    }, function () {
+        console.log('Error while loading source'); // Error!
+    });
+  }
+
+  componentWillUnmount() {
+    
+  }
+
+  render() {
+    return (
+      <div className="content">
+        <div className="player-wrapper">
+          <div id="player"></div>
+        </div>
+        <div className="description">
+          <p>For more information about the bitmovin player, please have a look at our online
+          <a href="//bitmovin.com/support" target="_blank">Developer Section</a>.</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 class Timer extends React.Component {
   constructor(props) {
@@ -34,31 +84,7 @@ class Timer extends React.Component {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        { 
-          //<img src={logo} className="App-logo" alt="logo" />
-        }
-        <p>
-          Edit <code>src/App.js</code> and save11 to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="content">
-        <div className="player-wrapper">
-          <div id="player"></div>
-        </div>
-        <div className="description">
-          <p>For more information about the bitmovin player, please have a look at our online
-          <a href="//bitmovin.com/support" target="_blank">Developer Section</a>.</p>
-        </div>
-      </div>
+      <PlayerWrapper />
       <Timer />
     </div>
   );
